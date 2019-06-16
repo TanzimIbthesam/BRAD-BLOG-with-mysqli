a00ad970abd39e7ca98ac9634267a605ac3a17c2
TanzimIbthesam <35595427+TanzimIbthesam@users.noreply.github.com>
2019-06-16 08:07:30 +0600

    Add files via upload

---
+++ BRADBLOG/inc/navbar.php
@@ -0,0 +1,35 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+			
+ </head>
+ <body>
+ <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button> -->
+          <a class="navbar-brand btn btn-info" href="index.php">Back</a>
+          <!-- <li><a href="index.php"class="btn btn-info">Home</a></li><br> -->
+        </div>
+        
+        
+          <ul class="nav navbar-nav">
+           
+            <li><a href="addpost.php"class="btn btn-info">Add Post</a></li>
+          </ul>
+        <!--/.nav-collapse -->
+      </div>
+    </nav>
+ </body>
+ </html>
+ 
